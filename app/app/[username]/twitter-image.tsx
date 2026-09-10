fd1edc2eb770e3a016bb53e12cbb80278b181839
prianshpate18 <priyansh.180804@gmail.com>
2026-09-10 12:00:31 +0530

    fix: declare twitter-image revalidate locally so next build can parse it

--- app/app/[username]/twitter-image.tsx
+++ app/app/[username]/twitter-image.tsx
@@ -1,2 +1,3 @@
 export const runtime = "nodejs";
-export { default, alt, size, contentType, revalidate } from "./opengraph-image";
+export const revalidate = 60;
+export { default, alt, size, contentType } from "./opengraph-image";
